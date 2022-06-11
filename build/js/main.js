edb73f44d369c203ff8b7a2b96cdf93ae94500e1
Pier-Yves Lessard <py.lessard@gmail.com>
2022-06-11 19:38:45 -0400

    Correctly reads config from browser and apply it

--- build/js/main.js
+++ build/js/main.js
@@ -5,10 +5,30 @@ var scrutiny_client_config = {
     }
 }
 
+function get_url_param(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
+
 function load_config() {
     if (typeof config_from_python !== 'undefined') {
         scrutiny_client_config = config_from_python
     }
+
+    let url_config = get_url_param('config')
+    if (url_config != '') {
+        try {
+            let json_utf8 = atob(url_config)
+            let config_from_url = JSON.parse(json_utf8)
+            scrutiny_client_config = config_from_url
+
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
 }
 
 (function() {
@@ -18,7 +38,8 @@ function load_config() {
     ui.init()
     let config = scrutiny_client_config
     server_conn = new ServerConnection(ui)
-    server_conn.start(config['server']['hostname'], config['server']['port'])
+    server_conn.set_endpoint(config['server']['hostname'], config['server']['port'])
+    server_conn.start()
 
     ui.register_widget(VarListWidget, server_conn)
     ui.register_widget(WatchWidget, server_conn)
